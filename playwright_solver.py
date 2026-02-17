653118ddf9ab0beb42ea5a4b8dc6acc3f688ab4e
Aleksander Olsvik <aleksanderolsvik02@gmail.com>
2026-02-17 21:11:48 +0100

    refactor to use setup function

--- playwright_solver.py
+++ playwright_solver.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
-from utils import get_todays_guess, load_json, guess_character
-from game_data import GameData
+from utils import get_todays_guess, guess_character
+from setup import setup_game
 from feedback import Feedback, FeedbackType
 import os
 
@@ -29,13 +29,7 @@ def generate_state_file():
         
 
 if __name__ == "__main__":
-    onepiece_data = load_json('onepiece_data.json')
-    character_data = onepiece_data['characters']
-    character_data = [{key.lower().replace(" ", "_"): value for key, value in character.items()} for character in character_data]
-    category_data = onepiece_data['categories']
-    category_data = {key.lower().replace(" ", "_"): value for key, value in category_data.items()}
-    game_data_options = {key.lower().replace(" ", "_") + "_options": list(value.get("options", [])) for key, value in category_data.items()}
-    game_data = GameData(**game_data_options)
+    game_data, character_data = setup_game()
     
     if not os.path.exists("state.json"):
         print("State file not found. Generating new state file...")
